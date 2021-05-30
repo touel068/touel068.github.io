c4a5e7934516c4c399307220ff36f1f5a868a605
Thomas Ouellette <43257395+touel068@users.noreply.github.com>
2021-05-30 18:07:14 -0400

    changes

--- TODO/scripts/groceries.js
+++ TODO/scripts/groceries.js
@@ -1,69 +1,145 @@
-	
+
 // Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products		 
 
 var products = [
 	{
 		name: "brocoli",
-        nutsFree:true,
-        lactoseFree:true,
+		nutsFree: true,
+		lactoseFree: true,
+		organic: true,
 		price: 1.99
 	},
 	{
 		name: "bread",
-        nutsFree:true,
-        lactoseFree:true,
+		nutsFree: true,
+		lactoseFree: true,
+		organic: false,
 		price: 2.35
 	},
 	{
 		name: "salmon",
-        nutsFree:true,
-        lactoseFree:true,
+		nutsFree: true,
+		lactoseFree: true,
+		organic: true,
 		price: 10.00
 	},
-    {
+	{
 		name: "nuts",
-        nutsFree:false,
-        lactoseFree:true,
+		nutsFree: false,
+		lactoseFree: true,
+		organic: false,
 		price: 2.30
 	},
-    {
-		name: "milk",
-        nutsFree:true,
-        lactoseFree:false,
+	{
+		name: "Pesto Sauce",
+		nutsFree: false,
+		lactoseFree: false,
+		organic: false,
+		price: 4.25
+	},
+	{
+		name: "Pasta",
+		nutsFree: true,
+		lactoseFree: true,
+		organic: true,
+		price: 5.35
+	},
+	{
+		name: "Cookies",
+		nutsFree: true,
+		lactoseFree: false,
+		organic: false,
 		price: 3.99
 	},
-  
+	{
+		name: "Cashews",
+		nutsFree: false,
+		lactoseFree: true,
+		organic: true,
+		price: 5.20
+	},
+	{
+		name: "Almond Milk",
+		nutsFree: false,
+		lactoseFree: true,
+		organic: false,
+		price: 6.10
+	},
+
+	{
+		name: "Orange Juice",
+		nutsFree: true,
+		lactoseFree: true,
+		organic: true,
+		price: 5.35
+	},
+
+
+
 ];
-	
+
 
 
 // given restrictions provided, make a reduced list of products
 // prices should be included in this list, as well as a sort based on price
 
-function restrictListProducts(prods, restriction) {
+function restrictListProducts(prods, restriction, restriction2) {
 	let product_names = [];
-	for (let i=0; i<prods.length; i+=1) {
-        if ((restriction == "NutsFree") && (prods[i].nutsFree == true)){
-			product_names.push(prods[i].name);
+
+	for (let i = 0; i < prods.length; i += 1) {
+		if ((restriction == "NutsFree") && (prods[i].nutsFree == true)) {
+			if ((restriction2 == "Organic") && (prods[i].organic == true)) {
+				product_names.push(prods[i].name);
+			}
+			else if (restriction2 == "NoneO") {
+				product_names.push(prods[i].name);
+			}
+
 		}
-        else if ((restriction == "LactoseFree") && (prods[i].lactoseFree == true)){
-			product_names.push(prods[i].name);
+		else if ((restriction == "LactoseFree") && (prods[i].lactoseFree == true)) {
+			if ((restriction2 == "Organic") && (prods[i].organic == true)) {
+				product_names.push(prods[i].name);
+			}
+			else if (restriction2 == "NoneO") {
+				product_names.push(prods[i].name);
+			}
 		}
-		else if (restriction == "None"){
-			product_names.push(prods[i].name);
+		else if (restriction == "None") {
+			if ((restriction2 == "Organic") && (prods[i].organic == true)) {
+				product_names.push(prods[i].name);
+			}
+			else if (restriction2 == "NoneO") {
+				product_names.push(prods[i].name);
+			}
 		}
 	}
+
+
+
 	return product_names;
 }
+
+
+
+
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
-	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name) > -1){
+	for (let i = 0; i < products.length; i += 1) {
+		if (chosenProducts.indexOf(products[i].name) > -1) {
 			totalPrice += products[i].price;
 		}
 	}
 	return totalPrice;
+}
+
+function getPrice(productName) {
+	for (let i = 0; i < products.length; i += 1) {
+		if (products[i].name === productName) {
+			return products[i].price;
+		}
+	}
+
 }
--- TODO/scripts/main.js
+++ TODO/scripts/main.js
@@ -22,20 +22,21 @@ function openInfo(evt, tabName) {
 
 }
 
-
-	
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
-    var s2 = document.getElementById(slct2);
-	
+function populateListProductChoices(slct2) {
+    var s1 = document.getElementById('dietSelect');
+	var s2 = document.getElementById(slct2);
+	var s3 = document.getElementById('organicSelect');
 	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
-    s2.innerHTML = "";
-		
+	s2.innerHTML = "";
+
+	products.sort((product1, product2) => (product1.price > product2.price ? 1 : -1));
+
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+	var optionArray = restrictListProducts(products, s1.value, s3.value);
+
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
@@ -44,6 +45,7 @@ function populateListProductChoices(slct1, slct2) {
 	for (i = 0; i < optionArray.length; i++) {
 			
 		var productName = optionArray[i];
+		var price = getPrice(productName);
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
@@ -53,8 +55,8 @@ function populateListProductChoices(slct1, slct2) {
 		
 		// create a label for the checkbox, and also add in HTML DOM
 		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
+		label.htmlFor = productName+ ' '+price + "$";
+		label.appendChild(document.createTextNode(productName+ ' '+price+"$"));
 		s2.appendChild(label);
 		
 		// create a breakline node and add in HTML DOM
